28b5700ac275cf84826c9a739c12af3f3d054751
Adnz <adnzwilr@hotmail.com>
2023-03-01 20:08:53 +0530

    understood the overflow problem

--- codes/python/myExp/myExp.py
+++ codes/python/myExp/myExp.py
@@ -2,22 +2,46 @@
 A Python module to calculate the exponential using Taylor Series
 exp(x) = summation(n=1, n=n) {x^n-1/(n-1)!}
 """
-from numpy import exp
+import numpy as np
 
-def expCal(x,n=100,):
+def expCal(x,n=100,convergence = False,automatic = True):
     """
     Actual Exponential function expCal()
     Inputs
     x: the number whose exp is to be calculated
     n: (default n=100) number of terms to calculate the value of function in taylor series.
+    convergence: to print convergence, set convergence to True.
+    automatic: to get correct value, n will be changed in automatic mode.
     """
 
-    sum = 0
-    for k in range(1,n):
-        sum = sum + x**(k-1) / factorial(k-1)
+    divisionValue = 25
+    if x > divisionValue:
+        [multi, addi] = np.divmod(x,divisionValue)
+        list1 = divisionValue*np.ones(multi)
+        list1 = np.append(list1,addi)
+    else:
+        list1 = x
 
-    print(f"error is equal to {exp(x)-sum}")
-    return sum
+    result = 1
+    for i in np.nditer(list1):
+        i = int(i)
+        sum = 0
+        for k in range(1,n):
+            sum = sum + i**(k-1) / factorial(k-1)
+            if convergence and k > 1:
+                conv = sum-sum2
+                print(f"Convergence of series after n term ={k} is {conv}")
+            sum2 = sum
+        result = result*sum
+        print(i)
+        print(result)
+
+
+
+
+
+    print(f"error is equal to {np.exp(x)-result}")
+    return result
     
 
 
@@ -29,8 +53,7 @@ def factorial(y):
     factor = 1
     for i in range(1,y+1):
         factor = factor*i
-
     return factor
 
 
-expCal(100)
+expCal(30)
