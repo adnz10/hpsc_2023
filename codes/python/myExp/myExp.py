e92544345330bd2021cdd16bd9d3eb34d18a688a
Adnz <adnzwilr@hotmail.com>
2023-03-02 19:00:25 +0530

    corrected convergence

--- codes/python/myExp/myExp.py
+++ codes/python/myExp/myExp.py
@@ -25,7 +25,7 @@ def expCal(x,n=100,convergence = False):
     high = range(n,0,-1)
     count = 0
     for k in np.nditer(x):
-        k = int(k)
+        k = float(k)
         for i,j in zip(low,high):
             A[count][i] = k**i/factorial(i)
             A[count][j] = k**j/factorial(j)
@@ -34,17 +34,18 @@ def expCal(x,n=100,convergence = False):
         count = count+1
 
     result = np.sum(A,1)
-    print(f"error is equal to {np.exp(x)-result}")
+    # print(f"error is equal to {np.exp(x)-result}")
     if convergence:
-         forward = A
-         print(forward)
-         backward = A
-         forward = np.delete(forward,0)
-         backward = np.delete(backward,-1)
-         filter = forward - backward
+        import matplotlib.pyplot as plt
+        filter = np.ravel(A)
+        filter = abs(np.convolve(filter,[1,-1],'same'))
+        ranges = range(1,filter.size +1) 
+        fig, ax = plt.subplots()
+        ax.plot(ranges,filter,label='Convergence')
+        plt.legend()
+        plt.show()
          
     return result
-    
 
 
 def factorial(y):
@@ -55,6 +56,7 @@ def factorial(y):
     assert y >= 0, "Factoriall is only defined for positive value"
     factor = 1
     for i in range(1,y+1):
+        i = float(i)
         factor = factor*i
     return factor
 
@@ -63,23 +65,12 @@ def plotting():
         """
         To see plot between custom and numpy exponential function 
         """
-        range = np.linspace(1,100,1000)
-        myExpCal = expCal(range)
-        numpyExpCal = np.exp(range)
+        ranges = np.linspace(1,100,1000)
+        myExpCal = expCal(ranges)
+        numpyExpCal = np.exp(ranges)
 
         fig, ax = plt.subplots()
-        ax.plot(range,numpyExpCal,label='Numpy Exp')
-        ax.scatter(range,myExpCal,color='C1',label='Custom Exp')
+        ax.plot(ranges,numpyExpCal,label='Numpy Exp')
+        ax.scatter(ranges,myExpCal,color='C1',label='Custom Exp')
         plt.legend()
         plt.show()
-        
-def test_main():
-	xvalues=[0., 2., 6]
-	for x in xvalues:
-		print(f"Testing with x={x}")
-		E = expCal(x)
-		E_numpy= np.exp(x)
-		print(f"myExp e^{x} = {E}, numpy e^{x} = {E_numpy}")
-		assert abs(E-E_numpy) < 1, "Your myExp function does not agree with numpy exp function"
-
-        
