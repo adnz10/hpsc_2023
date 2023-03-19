dd7c39aae6314f52b1111f53f30ac6bd46c7fba8
Adnz <adnzwilr@hotmail.com>
2023-03-19 10:08:36 +0530

    modified mysqrt function, debug

--- codes/Fortran/myFunc/mySqrt/mySqrt.f90
+++ codes/Fortran/myFunc/mySqrt/mySqrt.f90
@@ -3,26 +3,29 @@
 program main
         ! takes input for calculating sqrt
         implicit none
-        real(kind=8) :: x,y
-        print *,'Enter a number'
-        read *,x
+        real(kind=8) :: x,y,s0,s,tol,delta_s
+        integer :: i,kmax
+        logical :: debug
+        x = 64
+        debug = .true.
         if (x==0) then
                 y = 0
         elseif (x<0) then
                 print*,"Sqrt is not defined for negative number"
         else
-                call sqrtNt(x,y)
+                call sqrtNt(x,y,s0,s,tol,delta_s,i,kmax,debug)
                 print *,'Sqrt =',y
         endif
 
 end program
 
-subroutine sqrtNt(x,y)
+subroutine sqrtNt(x,y,s0,s,tol,delta_s,i,kmax,debug)
         implicit none
         real(kind=8),intent(in) :: x
         real(kind=8),intent(out) :: y
         real(kind=8)::s0,s,tol,delta_s
         integer :: i,kmax
+        logical :: debug
         s = 1
         kmax = 100
         tol = 1.d-14
@@ -30,6 +33,9 @@ subroutine sqrtNt(x,y)
                 s0 = s
                 s = 0.5*(s+x/s)
                 delta_s = s-s0
+                if (debug .eqv. .true.) then
+                        print *, "after iteration number",i,'sqrt of',x,'= ',s
+                endif
                 if( (abs(delta_s)/x) < tol) then
                   exit
           endif       
